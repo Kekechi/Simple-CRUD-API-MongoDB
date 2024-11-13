6c3811d76cfc455909222c225a8d9b79bf7d0670
Keigo Morita <>
2024-11-13 10:30:32 -0800

    simple todo api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import express from "express";
 const app = express();
 const port = 3000;
@@ -9,6 +9,77 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/todo/all", async (req, res) => {
+  try {
+    const todo = await client.db("todo").collection("todo").find({}).toArray();
+    res.send(todo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
+});
+
+app.get("/todo/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const todo = await client
+      .db("todo")
+      .collection("todo")
+      .findOne({ _id: new ObjectId(id) });
+    res.send(todo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
+});
+
+app.post("/todo/create", async (req, res) => {
+  try {
+    const body = req.body;
+    const todo = { name: body.name, status: "incomplete" };
+    await client.db("todo").collection("todo").insertOne(todo);
+    res.send("Inserted");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
+});
+
+app.post("/todo/:id/update", async (req, res) => {
+  try {
+    const body = req.body;
+    const id = req.params.id;
+
+    const updatedTodo = {
+      ...(body.name && { name: body.name }),
+      ...(body.status && { status: body.status }),
+    };
+
+    const result = await client
+      .db("todo")
+      .collection("todo")
+      .updateOne({ _id: new ObjectId(id) }, { $set: updatedTodo });
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
+});
+
+app.delete("/todo", async (req, res) => {
+  try {
+    const body = req.body;
+    await client
+      .db("todo")
+      .collection("todo")
+      .deleteOne({ _id: new ObjectId(body.id) });
+    res.send("Deleted");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
 });
 
 app.get("/test", async (req, res) => {
