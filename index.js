0e7d41e772545084640fdfc2c573192cfa5f44a7
Keigo Morita <>
2024-11-13 10:31:13 -0800

    remove test end points

--- index.js
+++ index.js
@@ -82,32 +82,6 @@ app.delete("/todo", async (req, res) => {
   }
 });
 
-app.get("/test", async (req, res) => {
-  const query = req.query;
-  const test = await client
-    .db("test")
-    .collection("test")
-    .findOne({ name: query.name });
-  res.send(test);
-});
-
-app.post("/test", async (req, res) => {
-  const body = req.body;
-  const test = { name: body.name, type: body.type };
-  await client.db("test").collection("test").insertOne(test);
-  res.send("Inserted");
-});
-
-app.post("/test/update", async (req, res) => {
-  const body = req.body;
-  const test = { name: body.name, type: body.type };
-  await client
-    .db("test")
-    .collection("test")
-    .updateOne({ name: body.name }, { $set: { type: body.type } });
-  res.send("Updated");
-});
-
 client
   .connect()
   .catch((err) => console.log(`error connecting to MongoDB\n${err}`))
